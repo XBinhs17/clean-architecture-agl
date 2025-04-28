7a98526b4fa9638974536fbbf5ae99ed7de062e5
XBinhs17 <nguyenbinh17062003@gmail.com>
2025-04-28 13:16:00 +0700

    rebuild struct

--- src/app/core/models/target.model.ts
+++
@@ -1,9 +0,0 @@
-export interface TargetItem{
-  position: string;
-  name: string;
-  description: string;
-  avatars: string[];
-  status: string;
-  file: number,
-  message: number
-}
--- src/app/core/models/user.model.ts
+++
@@ -1,9 +0,0 @@
-export interface UserItem{
-  id: number;
-  avatar: string;
-  name: string;
-  work: string;
-  earnings: number;
-  tasks: number;
-  sales: number;
-}
