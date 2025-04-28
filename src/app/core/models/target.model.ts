1a834e7657bead86d26493cfd7f71abff947b689
XBinhs17 <nguyenbinh17062003@gmail.com>
2025-04-28 09:08:10 +0700

    first commit

---
+++ src/app/core/models/target.model.ts
@@ -0,0 +1,9 @@
+export interface TargetItem{
+  position: string;
+  name: string;
+  description: string;
+  avatars: string[];
+  status: string;
+  file: number,
+  message: number
+}
---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,9 @@
+export interface UserItem{
+  id: number;
+  avatar: string;
+  name: string;
+  work: string;
+  earnings: number;
+  tasks: number;
+  sales: number;
+}
